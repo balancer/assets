4996aae7be68ec9270af45fe5f4830a07a43e83f
Timur Badretdinov <destinerx@gmail.com>
2020-11-02 12:24:14 +0300

    Add logo URLs for Kovan test assets

--- scripts/generate.js
+++ scripts/generate.js
@@ -216,6 +216,7 @@ function getColor(network, address, data) {
 }
 
 function getLogoUrl(assets, address) {
+	address = getMainnetAddress(address);
 	if (address === 'ether') {
 		return 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png'
 	}
@@ -226,6 +227,22 @@ function getLogoUrl(assets, address) {
 		return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`;
 	}
 	return '';
+}
+
+function getMainnetAddress(address) {
+	const map = {
+		'0x1528F3FCc26d13F7079325Fb78D9442607781c8C': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+		'0xef13C0c8abcaf5767160018d268f9697aE4f5375': '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
+		'0x2F375e94FC336Cdec2Dc0cCB5277FE59CBf1cAe5': '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+		'0x8c9e6c40d3402480ACE624730524fACC5482798c': '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+		'0xe0C9275E44Ea80eF17579d33c55136b7DA269aEb': '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+		'0xd0A1E359811322d97991E03f863a0C30C2cF029C': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+		'0x1f1f156E0317167c11Aa412E3d1435ea29Dc3cCE': '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
+		'0x86436BcE20258a6DcfE48C9512d4d49A30C4d8c4': '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F',
+		'0x37f03a12241E9FD3658ad6777d289c3fb8512Bc9': '0x960b236A07cf122663c4303350609A66A7B288C0',
+		'0xccb0F4Cf5D3F97f4a55bb5f5cA321C3ED033f244': '0xE41d2489571d322189246DaFA5ebDe1F4699F498',
+	};
+	return map[address] || address;
 }
 
 function mergeTokenLists(lists) {
