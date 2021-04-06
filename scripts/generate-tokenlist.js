b31e235b5f7f19e25d87c2d62cb8959944a55310
Fabien <bonustrack@users.noreply.github.com>
2021-04-06 21:03:09 +0700

    Add success msg (#276)
    * Use global env
    
    * Add success message
    
    * Throw error

--- scripts/generate-tokenlist.js
+++ scripts/generate-tokenlist.js
@@ -65,11 +65,12 @@ async function generate(name, tokens) {
 	if (fleekApiSecret) {
 		try {
 			await ipfsPin(`assets/${name}.tokenlist.json`, list);
+			console.log(`Tokenlist uploaded for ${name}`);
 		} catch (e) {
-			console.error('Failed to pin list on IPFS', e);
+			throw new Error('Failed to pin list on IPFS');
 		}
 	} else {
-		console.error('Fleek API secret is missing');
+		throw new Error('Fleek API secret is missing');
 	}
 }
 
